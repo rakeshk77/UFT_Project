28108ab0862d580121ff12830f7451c0fbc6f415
rkrishnan007 <c-rkrishnan@denaliai.com>
2025-01-13 10:37:57 +0530

    Modified code

--- web.bat
+++ web.bat
@@ -1,9 +1,13 @@
 @echo off
-set UFT_PATH="C:\Program Files (x86)\OpenText\UFT One\bin\UFTBatchRunnerCMD.exe"
-set TEST_SOURCE="D:\UFT Project\Web\Tests\Login"
-set RESULT_PATH="D:\UFT Project\Web\Tests\Login"
+set "UFT_PATH=C:\Program Files (x86)\OpenText\UFT One\bin\UFTBatchRunnerCMD.exe"
+set "TEST_SOURCE=D:\UFT Project\Web\Tests\Login"
+set "RESULT_PATH=D:\UFT Project\Web\Tests\Login"
 
 echo Running UFT test...
-call %UFT_PATH% -source %TEST_SOURCE% -result %RESULT_PATH%
-echo Exit Code: %ERRORLEVEL%
-exit /b %ERRORLEVEL%
+call "%UFT_PATH%" -source "%TEST_SOURCE%" -result "%RESULT_PATH%"
+if %ERRORLEVEL%==-532462766 (
+    echo Ignoring error code -532462766 and marking as success.
+    exit /b 0
+) else (
+    exit /b %ERRORLEVEL%
+)
